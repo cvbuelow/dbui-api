14bf136368d931cffa08c97ec9e9a1562c071b80
Carl von Buelow <cvbuelow@gmail.com>
2014-05-30 17:11:51 -0400

    Working now but script not ending

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var Tunnel = require('tunnel-ssh');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
 var config = {
-    remotePort: 3306, //localport
-    localPort: 8181, //remoteport
+    remotePort: 3306, 
+    localPort: 8182, 
     verbose: true, // dump information to stdout
-    disabled: false, //set this to true to disable tunnel (useful to keep architecture for local connections)
-    sshConfig: { //ssh2 configuration (https://github.com/mscdex/ssh2)
+    sshConfig: {
       host: 'mileschristi.org',
       port: 22,
       username: 'mcwebadmin',
@@ -17,22 +16,14 @@ var config = {
 var tunnel = new Tunnel(config);
 tunnel.connect(function (error) {
     console.log(error);
-    //or start your remote connection here .... 
-    //mongoose.connect(...);
-    /*var connection = mysql.createConnection({
+
+    var connection = mysql.createConnection({
       host     : '127.0.0.1',
       database : 'mcwebadmin',
       user     : 'mcwebadmin',
       password : 'manresa1556',
-      port     : 8181
-    });*/
-
-    var connection = mysql.createConnection({
-      host     : '127.0.0.1',
-      database : 'cmshost',
-      user     : 'root',
-      password : 'root',
-      port     : 8889
+      port     : config.localPort,
+      insecureAuth: true
     });
 
     connection.connect(function(err) {
@@ -40,10 +31,13 @@ tunnel.connect(function (error) {
         console.error('error connecting: ' + err.stack);
         return;
       }
-      console.log('connected as id ' + connection.threadId);
     });
+    
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows) {
+      if (err) throw err;
+      console.log('The solution is: ', rows[0].solution);
+    });
+    
+    connection.end(tunnel.close);
 
-
-    //close tunnel to exit script 
-    tunnel.close();
 });
